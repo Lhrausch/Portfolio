a4d55603488e5a83571fd5d84bae12186027c850
lhrausch <lukerausch@gmail.com>
2022-01-31 15:26:04 -0600

    minor edits fixing small bugs. resume modal still broken

--- jsbio.js
+++ jsbio.js
@@ -1,6 +1,26 @@
 console.log("test");
 
 $(() => {
+    const $openBtn = $("#open-resume")
+    const $modal = $("#modal")
+    const $closeBtn = $("#close-resume")
+
+
+    const openResume = () => {
+        $modal.css("display", "block")
+    }
+
+    const closeResume = () => {
+        $modal.css("display", "none")
+    }
+
+    $openBtn.on("click", openResume)
+    $closeBtn.on("click", closeResume)
+
+    // const $download = $("#download-resume")
+
+    // $download.on("click", )
+
 
     currentImageIndex = 0
     numOfImages = $(".carousel-images").children().length - 1
@@ -22,6 +42,15 @@ $(() => {
         currentImageIndex = numOfImages
         }
         $(".carousel-images").children().eq(currentImageIndex).css("display", "block")
+
+
+        // $(".proj-1").on("click"), () =>  {
+        //     figure out how to open project into $modal, once project completed.. lol
+        // or just create new page to house it
+        // }
+
+
+
     })
 
 });
